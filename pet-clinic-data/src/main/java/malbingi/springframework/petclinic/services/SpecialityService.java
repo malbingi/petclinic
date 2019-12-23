667bf7e1966f275dcaf8bb91dd0f104e5a9150e1
malbingi <mateuszalbingier53@gmail.com>
2019-12-23 15:33:29 +0100

    Added SpecialtyMapService. Closes #31

---
+++ pet-clinic-data/src/main/java/malbingi/springframework/petclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package malbingi.springframework.petclinic.services;
+
+import malbingi.springframework.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
