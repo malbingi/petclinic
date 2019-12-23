521b11f709ae4ec7408454a0cedddc215986912e
malbingi <mateuszalbingier53@gmail.com>
2019-12-23 14:10:52 +0100

    Added PetType Map Service Impl. Closes #29

---
+++ pet-clinic-data/src/main/java/malbingi/springframework/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package malbingi.springframework.petclinic.services;
+
+import malbingi.springframework.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
